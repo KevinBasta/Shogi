4bd493b55a83973ea4fcb947d9352b6d40e9045f
KevinBasta <Kevin.Basta2@gmail.com>
2022-02-10 15:59:58 -0500

    some structural planning
    wish I can work on this more right now but I have a lot of other stuff to do :(

--- main.js
+++ main.js
@@ -11,3 +11,20 @@ function logBoard() {
     lastBoardState = logNumber;
     boardLogs[("log_" + logNumber)] = ['lol'];
 }
+
+
+/* 2 player objects created that have the gote or sente property
+   to allow for piece movemnts in online multiplayer, offline multiplayer, 
+   and player vs computer. 
+
+   The player class can have the properties of pices in possetion, pices captured, etc..
+   
+   the second player can have the property of a real player or a bot or 
+   it can also be a different computer player object that gets initialized instead
+   of a player or maybe it's just inheritance of the class player.
+   
+   look up the input/output for already existing shogi bots to integrate in design
+   */ 
+
+/* game object created at the start of each game that takes the mode and 
+   determiens how to set stuff up. */
