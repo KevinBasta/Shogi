53f84d2b3da5dc2ad3338d5d62bfe7f80535decb
KevinBasta <Kevin.Basta2@gmail.com>
2022-02-16 10:25:41 -0500

    oop setup

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-const boardSetup = {
-
+const defultBoardSetup = {
+    /* https://en.wikipedia.org/wiki/Shogi */
+    gote_Lance_1: "", 
+    gote_Silver
 }
 
 const picesImages = { 
---
+++ player.js
@@ -0,0 +1,16 @@
+class player { 
+    constructor(gote_sente, player_bot) { 
+        this.gote_sente = gote_sente;
+        this.player_bot = player_bot;
+        this.pices;
+    }
+
+    movePiece(piece, newPosition) {
+
+    }
+
+    getGoteSente() { 
+        return this.gote_sente;
+    }
+
+}
